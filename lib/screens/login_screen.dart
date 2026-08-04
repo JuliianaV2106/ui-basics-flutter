b6416b4d987abe0d590fc2c41d6e2ef6b7889f49
Juliana Filigrana Valencia <juliianavalenciia21@gmail.com>
2026-08-04 15:13:47 -0500

    Create login screen based on Figma design

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../utils/app_colors.dart';
 import '../widgets/custom_text_field.dart';
 import '../widgets/primary_button.dart';
 
@@ -9,95 +10,103 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
+
       body: SafeArea(
         child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 24,
-            vertical: 30,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 28,
+              vertical: 20,
+            ),
 
-              const SizedBox(height: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
 
-              const Icon(
-                Icons.flutter_dash,
-                size: 90,
-                color: Colors.blue,
-              ),
+                const SizedBox(height: 40),
 
-              const SizedBox(height: 30),
-
-              const Text(
-                "Welcome Back",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-
-              const SizedBox(height: 10),
-
-              const Text(
-                "Login to continue",
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                ),
-              ),
-
-              const SizedBox(height: 40),
-
-              const CustomTextField(
-                label: "Email",
-                hint: "Enter your email",
-                prefixIcon: Icons.email_outlined,
-              ),
-
-              const SizedBox(height: 20),
-
-              const CustomTextField(
-                label: "Password",
-                hint: "Enter your password",
-                prefixIcon: Icons.lock_outline,
-                obscureText: true,
-              ),
-
-              const SizedBox(height: 10),
-
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text("Forgot password?"),
-                ),
-              ),
-
-              const SizedBox(height: 20),
-
-              PrimaryButton(
-                text: "Login",
-                onPressed: () {},
-              ),
-
-              const SizedBox(height: 30),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-
-                  const Text("Don't have an account?"),
-
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text("Register"),
+                Center(
+                  child: Image.asset(
+                    "assets/images/login.png",
+                    height: 220,
                   ),
+                ),
 
-                ],
-              )
+                const SizedBox(height: 40),
 
-            ],
+                const Text(
+                  "Welcome Back",
+                  style: TextStyle(
+                    fontSize: 34,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                const SizedBox(height: 10),
+
+                Text(
+                  "Sign in to continue",
+                  style: TextStyle(
+                    color: AppColors.grey,
+                    fontSize: 18,
+                  ),
+                ),
+
+                const SizedBox(height: 40),
+
+                const CustomTextField(
+                  hint: "Email",
+                  icon: Icons.email_outlined,
+                ),
+
+                const SizedBox(height: 20),
+
+                const CustomTextField(
+                  hint: "Password",
+                  icon: Icons.lock_outline,
+                  obscure: true,
+                ),
+
+                const SizedBox(height: 10),
+
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Forgot Password?",
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                PrimaryButton(
+                  text: "Login",
+                  onPressed: () {},
+                ),
+
+                const SizedBox(height: 30),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    const Text(
+                      "Don't have an account?",
+                    ),
+
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("Sign Up"),
+                    )
+
+                  ],
+                )
+
+              ],
+            ),
           ),
         ),
       ),
